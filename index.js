376824621a41b28c8acbea6f66e7918a1b8a8ec3
stevetoddy <stevetoddy@gmail.com>
2023-01-25 13:51:16 +1100

    Refactor routes into routes folder

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-import app from "./app.js"
+import express from 'express'
+import userRoutes from './routes/user_routes.js'
+import programRoutes from './routes/program_routes.js'
 
+
+// Declare express under 'app' and assign a port number
+const app = express()
 const port = process.env.PORT || 4001
+
+
+// Parses incoming requests with JSON headers
+app.use(express.json())
+
+
+// Route Ends
+app.use('/users', userRoutes)
+app.use('/programs', programRoutes)
+
+
+// app.get/.put/.post etc is middle ware that comes with express and needs to go before 'app.listen'
+// First parameter is the path, and can take a callback function second, which takes two parameters 'request' and 'response' (req, res)
+// We can send responses using 'response.send()'.... This can be HTML, objects etc..
+app.get('/', (req, res) => res.status(200).send({info: `Physio App 2023`}))
+
 
 // Tell express to listen for connections (default is localhost)
 // Can add a callback function, in this case a console log showing the URL
---
+++ routes/user_routes.js
@@ -0,0 +1,64 @@
+import { Router } from 'express'
+import { UserModel } from "../db.js"
+
+const router = Router()
+
+// USER ROUTES
+// Retrieve all Users
+router.get('/:email', async (req, res) => res.status(200).send(await UserModel.find({ email: req.params.email })))
+
+// Create an User
+router.post('/signup', async (req, res) => {
+    try {
+        const { username, email, password } = req.body
+
+        const newUser = { username, email, password }
+
+        const insertedUser = await UserModel.create(newUser)
+
+        res.status(201).send(insertedUser)     
+    }
+    catch (err) {
+         res.status(500).send({ error: err.message })
+    }
+})
+
+// Update an User
+router.put('/:id', async (req, res) => {
+    
+    const { username, email, password } = req.body
+    // Validation/sanitize inputs here
+    const updateUser = { username, email, password }
+
+    try {
+        const user = await UserModel.findByIdAndUpdate(req.params.id, updateUser, { returnDocument: 'after' })
+
+        if (user) {
+            res.send(await user.populate()) // MIGHT NEED TO CHECK
+        } else {
+            res.status(404).send({ error: 'User not found' })
+        }
+    }
+    catch (err) {
+        res.status(500).send({ error: err.message })
+    }
+})
+
+// Delete User
+router.delete('/:id', async (req, res) => {
+    try {
+        const user = await UserModel.findByIdAndDelete(req.params.id)
+
+        if (user) {
+            res.status(204)
+        } else {
+            res.status(404).send({ error: 'Entry not found' })
+        }
+    }
+    catch (err) {
+        res.status(500).send({ error: err.message })
+    }
+})
+
+
+export default router
