f475a2e6c90cb978f71fb8b4073890119f422e91
OliWongDev <14wongom@gmail.com>
2023-01-27 15:05:43 +1100

    Test/Auth Fix

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -5,35 +5,6 @@ import users from '../routes/user_routes.js'
 import jwt from 'jsonwebtoken'
 import authenticateToken from '../controllers/auth_controller'
 
-
-// describe('Test the user routes', () => {
-//   beforeEach(async () => {
-//       const testUser = await UserModel.create({email: "test@gmail.com", username: "testuser", password: "testpassword"})
-//     //   const token = await jwt.sign({ testUser }, 
-//     //     // Below, use an ENV reference
-//     //     "nfb32iur32ibfqfvi3vf932bg932g932", 
-//     //     { expiresIn: 360000 })
-//   })
-
-//   afterEach(async () => {
-//       await UserModel.deleteMany()
-//   })
-
-//   test('It should return all users', async () => {
-//       app.use('/users', programs)
-//       const token = await jwt.sign({ testUser }, 
-//         // Below, use an ENV reference
-//         "nfb32iur32ibfqfvi3vf932bg932g932", 
-//         { expiresIn: 360000 })
-//       const response = await request(app)
-//         .get('/')
-//         .set('Authorization', token)
-//       expect(response.status).toBe(200)
-//       expect(response.body.length).toBe(1)
-//       expect(response.body[0].username).toBe("testuser")
-//   })
-// })
-
 describe('Test the user routes', () => {
     let testUser, token
 
@@ -43,7 +14,6 @@ describe('Test the user routes', () => {
         // Below, use an ENV reference
         "nfb32iur32ibfqfvi3vf932bg932g932", 
         { expiresIn: 360000 })
-    console.log(token)
     })
 
     afterEach(async () => {
@@ -51,10 +21,9 @@ describe('Test the user routes', () => {
     })
 
     test('It should return all users', async () => {
-        app.use('/', users)
         const response = await request(app)
                 .get('/users')
-                .set({ 'Authorization': `Bearer ${token}`})
+                .set({ 'authorization': `Bearer ${token}`})
               expect(response.status).toBe(200)
               expect(response.body.length).toBe(5)
               expect(response.body[4].username).toBe("testuser")
--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken'
 
 function authenticateToken(req, res, next) {
-    const authHeader = req.headers['Authorization']
+    const authHeader = req.get('authorization')
     const token = authHeader && authHeader.split(' ')[1] 
     
     if (token == null) return res.status(401).send({ "error": "Lacks Valid Authentication" })
