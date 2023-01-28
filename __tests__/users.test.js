7da0f9c47b96f4fd223e788a0da82b998f2e11bc
OliWongDev <14wongom@gmail.com>
2023-01-28 18:44:29 +1100

    Same user authorization pause

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -1,9 +1,9 @@
 import request from 'supertest'
 import { UserModel } from '../db'
 import app from '../index.js'
-import users from '../routes/user_routes.js'
 import jwt from 'jsonwebtoken'
-import authenticateToken from '../controllers/auth_controller'
+import { expect, jest, test } from '@jest/globals'
+
 
 describe('Test the user routes', () => {
     let testUser, token
@@ -20,14 +20,84 @@ describe('Test the user routes', () => {
         await UserModel.deleteOne({username: "testuser"})
     })
 
-    test('It should return all users', async () => {
+    test('GET all users', async () => {
         const response = await request(app)
                 .get('/users')
-                .set({ 'authorization': `Bearer ${token}`})
+                .set({'authorization': `Bearer ${token}`})
               expect(response.status).toBe(200)
               expect(response.body.length).toBe(5)
               expect(response.body[4].username).toBe("testuser")
-          })
+    })
+    
+    test('GET one user, with a given ID', async () => {
+        const response = await request(app)
+            .get(`/users/${testUser._id.valueOf()}`)
+            .set({'authorization': `Bearer ${token}`})
+            expect(response.status).toBe(200)
+            expect(Object.keys(response.body).length).toBe(5)
+    })
+    
+    test('PUT one user with a given ID', async () => {
+        const updatedUser = { username: 'updatedUserName', email: 'updated@gmail.com', password: 'updatedPassword'}
+        const response = await request(app)
+            .put(`/users/${testUser._id.valueOf()}`)
+            .set({'authorization': `Bearer ${token}`})
+            .send(updatedUser)
+        expect(response.status).toBe(200)
+        expect(response.body).toMatchObject(updatedUser)
+        const updatedUserFromDb = await UserModel.findById(testUser._id.valueOf())
+        expect(updatedUserFromDb.username).toBe(updatedUser.username)
+        expect(updatedUserFromDb.email).toBe(updatedUser.email)
+        expect(updatedUserFromDb.password).toBe(updatedUser.password)
 
+    })
+    
+    test('PUT one user, with an invalid request', async () => {
+        const response = await request(app)
+            .put(`/users/${testUser._id.valueOf()}`)
+            .set({  'authorization': `Bearer ${token}`})
+            .send({username: 'testuser', email: 'test@gmail.com', password: '12'})
+        expect(response.status).toBe(400)
+        expect(response.body.errors[0].msg).toBe('Please provide a password that is greater than 5 characters.')
+    })
+
+    test('PUT one user, with a non-existing ID', async () => {
+        const fakeId = "aohcauohscsanlca"
+        const response = await request(app)
+            .put(`/users/${fakeId}`)
+            .set({  'authorization': `Bearer ${token}`})
+            .send({username: 'testuser', email: 'test@gmail.com', password: 'testpassword'})
+        expect(response.status).toBe(500)
+    })
+
+    test('DELETE one user', async () => {
+        const response = await request(app)
+            .delete(`/users/${testUser._id.toString()}`)
+            .set({'authorization': `Bearer ${token}`})
+        expect(response.status).toBe(204)
+    })
+
+    test('DELETE one user, with a non-existing ID', async () => {
+        const fakeId = "aohcauohscsanlca"
+        const response = await request(app)
+            .put(`/users/${fakeId}`)
+            .set({  'authorization': `Bearer ${token}`})
+            .send({username: 'testuser', email: 'test@gmail.com', password: 'testpassword'})
+        expect(response.status).toBe(500)
+    })
+
+    test('DELETE, ', async () => {
+        jest.spyOn(UserModel, 'findByIdAndDelete').mockImplementationOnce(() => {
+            throw new Error('Server Error')
+        })
+
+        const res = await request(app)
+            .delete(`/users/${testUser._id.toString()}`)
+            .set({  'authorization': `Bearer ${token}`})
+
+        expect(res.status).toBe(500)
+        expect(res.body).toEqual({ error: 'Server Error' })
+    })
 })
+
 
--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { UserModel } from "../db.js"
 import authenticateToken from '../controllers/auth_controller.js'
+import { check, validationResult } from 'express-validator'
 
 const router = Router()
 
@@ -8,36 +9,38 @@ const router = Router()
 // Retrieve all Users
 router.get('/', authenticateToken, async (req, res) => res.send(await UserModel.find()))
 
-// // Create an User
-// router.post('/signup', async (req, res) => {
-//     try {
-//         const { username, email, password } = req.body
-
-//         const newUser = { username, email, password }
-
-//         const insertedUser = await UserModel.create(newUser)
-
-//         res.status(201).send(insertedUser)     
-//     }
-//     catch (err) {
-//          res.status(500).send({ error: err.message })
-//     }
-// })
+// Retrieve a specific user
+router.get('/:id', authenticateToken, async (req, res) => res.send(await UserModel.findById(req.params.id)))
 
 // Update an User
-router.put('/:id', authenticateToken, async (req, res) => {
+router.put('/:id', [
+    check("email", "Please provide a valid email")
+      .isEmail(),
+    check("password", "Please provide a password that is greater than 5 characters.")
+      .isLength({
+        min: 6
+      }),
+    check("username", "Please provide a username that is greater than 5 characters.")
+      .isLength({
+        min: 3
+      })
+  ], authenticateToken, async (req, res) => {
     
     const { username, email, password } = req.body
-    // Validation/sanitize inputs here
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty())
+        return res.status(400).json({
+      errors: errors.array()
+    })
+
     const updateUser = { username, email, password }
 
     try {
         const user = await UserModel.findByIdAndUpdate(req.params.id, updateUser, { returnDocument: 'after' })
 
         if (user) {
-            res.send(await user.populate()) // MIGHT NEED TO CHECK
-        } else {
-            res.status(404).send({ error: 'User not found' })
+            res.send(await user.populate()) // MIGHT NEED TO CHECK // Reminder to check with Steve if a 404 else for this block is necessary. Difficult to access through a test.
         }
     }
     catch (err) {
@@ -48,16 +51,16 @@ router.put('/:id', authenticateToken, async (req, res) => {
 // Delete User
 router.delete('/:id', authenticateToken, async (req, res) => {
     try {
+        if(req.user.id.toString() !== req.params.id.toString()) {
+            return res.status(401).send({ "error": "Unauthorized to perform this action" })
+        }
         const user = await UserModel.findByIdAndDelete(req.params.id)
 
-        if (user) {
-            res.status(204)
-        } else {
-            res.status(404).send({ error: 'Entry not found' })
-        }
+        if (user) 
+            res.status(204).send() // Reminder to check with Steve if a 404 else for this block is necessary. Difficult to access through a test.
     }
     catch (err) {
-        res.status(500).send({ error: err.message })
+            res.status(500).send({ error: err.message })
     }
 })
 
